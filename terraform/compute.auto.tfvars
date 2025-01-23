df1f680e4e33bda720cf12ac8f74b9ca9d6f6dd8
Thomas Mauran <thomas.mauran@etu.umontpellier.fr>
2025-01-23 09:31:46 +0100

    feat: tftlp if

--- terraform/compute.auto.tfvars
+++ terraform/compute.auto.tfvars
@@ -9,46 +9,46 @@ k3s_token = "K10ecf1ba02dc1b04df10daa949058e4c78583b0c06d5b07c5bf108a26ba5b6ad52
 master_ip = "192.168.1.40"
 
 vm_instances = {
-    "master" = {
-        name = "master",
-        playbook = "k3s-master",
-        ip = "192.168.1.40"
-    },
+    # "master" = {
+    #     name = "master",
+    #     playbook = "k3s-master",
+    #     ip = "192.168.1.40"
+    # },
 
 
-    "node1" = {
-        name = "node1",
-        playbook = "k3s-node",
-        ip = "192.168.1.10"
-    }
-
-    "node2" = {
-        name = "node2",
-        playbook = "k3s-node",
-        ip = "192.168.1.20"
-    }
-
-
-
-    # "lb" = {
-    #     name = "lb",
-    #     playbook = "lb",
+    # "node1" = {
+    #     name = "node1",
+    #     playbook = "k3s-node",
     #     ip = "192.168.1.10"
-    # },
-    # "backend" = {
-    #     name = "backend",
-    #     playbook = "backend",
-    #     ip = "192.168.1.40",
     # }
-    # "front1" = {
-    #     name = "front1",
-    #     playbook = "frontend",
-    #     ip = "192.168.1.20",
-    # },
-    # "front2" = {
-    #     name = "front2",
-    #     playbook = "frontend",
-    #     ip = "192.168.1.30",
-    # },
+
+    # "node2" = {
+    #     name = "node2",
+    #     playbook = "k3s-node",
+    #     ip = "192.168.1.20"
+    # }
+
+
+
+    "lb" = {
+        name = "lb",
+        playbook = "lb",
+        ip = "192.168.1.50"
+    },
+    "backend" = {
+        name = "backend",
+        playbook = "backend",
+        ip = "192.168.1.60",
+    }
+    "front1" = {
+        name = "front1",
+        playbook = "frontend",
+        ip = "192.168.1.70",
+    },
+    "front2" = {
+        name = "front2",
+        playbook = "frontend",
+        ip = "192.168.1.80",
+    },
 
 }
--- terraform/modules/security_group/main.tf
+++ terraform/modules/security_group/main.tf
@@ -23,22 +23,55 @@ resource "openstack_networking_secgroup_rule_v2" "allow_http" {
   remote_ip_prefix  = "0.0.0.0/0"
 }
 
-resource "openstack_networking_secgroup_rule_v2" "allow_kube" {
+# Allow K3s API Server
+resource "openstack_networking_secgroup_rule_v2" "allow_kube_api" {
   security_group_id = openstack_networking_secgroup_v2.vm_security_group.id
   direction         = "ingress"
   ethertype         = "IPv4"
-  port_range_min    = "6443"
-  port_range_max    = "6443"
+  port_range_min    = 6443
+  port_range_max    = 6443
   protocol          = "tcp"
   remote_ip_prefix  = "0.0.0.0/0"
 }
 
-resource "openstack_networking_secgroup_rule_v2" "allow_kube_egress" {
+# Allow K3s Flannel VXLAN Networking
+resource "openstack_networking_secgroup_rule_v2" "allow_flannel_vxlan" {
+  security_group_id = openstack_networking_secgroup_v2.vm_security_group.id
+  direction         = "ingress"
+  ethertype         = "IPv4"
+  protocol          = "udp"
+  port_range_min    = 8472
+  port_range_max    = 8472
+  remote_ip_prefix  = "0.0.0.0/0"
+}
+
+# Allow NodePort Services
+resource "openstack_networking_secgroup_rule_v2" "allow_nodeport" {
+  security_group_id = openstack_networking_secgroup_v2.vm_security_group.id
+  direction         = "ingress"
+  ethertype         = "IPv4"
+  protocol          = "tcp"
+  port_range_min    = 30000
+  port_range_max    = 32767
+  remote_ip_prefix  = "0.0.0.0/0"
+}
+
+# Allow kubelet API
+resource "openstack_networking_secgroup_rule_v2" "allow_kubelet" {
+  security_group_id = openstack_networking_secgroup_v2.vm_security_group.id
+  direction         = "ingress"
+  ethertype         = "IPv4"
+  port_range_min    = 10250
+  port_range_max    = 10250
+  protocol          = "tcp"
+  remote_ip_prefix  = "0.0.0.0/0"
+}
+
+# Allow Egress Traffic (General)
+resource "openstack_networking_secgroup_rule_v2" "allow_egress" {
   security_group_id = openstack_networking_secgroup_v2.vm_security_group.id
   direction         = "egress"
   ethertype         = "IPv4"
-  port_range_min    = "6443"
-  port_range_max    = "6443"
   protocol          = "tcp"
   remote_ip_prefix  = "0.0.0.0/0"
 }
