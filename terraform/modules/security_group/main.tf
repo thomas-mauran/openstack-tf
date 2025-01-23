dba5f85a59ad2a223dd76363e8f062301deae1ee
Thomas Mauran <thomas.mauran@etu.umontpellier.fr>
2025-01-23 11:31:10 +0100

    fix: k3s

--- terraform/modules/security_group/main.tf
+++ terraform/modules/security_group/main.tf
@@ -23,6 +23,17 @@ resource "openstack_networking_secgroup_rule_v2" "allow_http" {
   remote_ip_prefix  = "0.0.0.0/0"
 }
 
+# Allow embedded etcd (HA)
+resource "openstack_networking_secgroup_rule_v2" "allow_embedded_etcd" {
+  security_group_id = openstack_networking_secgroup_v2.vm_security_group.id
+  direction         = "ingress"
+  ethertype         = "IPv4"
+  port_range_min    = 2379
+  port_range_max    = 2380
+  protocol          = "tcp"
+  remote_ip_prefix  = "0.0.0.0/0"
+}
+
 # Allow K3s API Server
 resource "openstack_networking_secgroup_rule_v2" "allow_kube_api" {
   security_group_id = openstack_networking_secgroup_v2.vm_security_group.id
@@ -34,7 +45,7 @@ resource "openstack_networking_secgroup_rule_v2" "allow_kube_api" {
   remote_ip_prefix  = "0.0.0.0/0"
 }
 
-# Allow K3s Flannel VXLAN Networking
+# Allow Flannel VXLAN Networking
 resource "openstack_networking_secgroup_rule_v2" "allow_flannel_vxlan" {
   security_group_id = openstack_networking_secgroup_v2.vm_security_group.id
   direction         = "ingress"
@@ -42,6 +53,50 @@ resource "openstack_networking_secgroup_rule_v2" "allow_flannel_vxlan" {
   protocol          = "udp"
   port_range_min    = 8472
   port_range_max    = 8472
+  remote_ip_prefix  = "0.0.0.0/0"
+}
+
+# Allow Flannel WireGuard (IPv4)
+resource "openstack_networking_secgroup_rule_v2" "allow_flannel_wireguard_ipv4" {
+  security_group_id = openstack_networking_secgroup_v2.vm_security_group.id
+  direction         = "ingress"
+  ethertype         = "IPv4"
+  protocol          = "udp"
+  port_range_min    = 51820
+  port_range_max    = 51820
+  remote_ip_prefix  = "0.0.0.0/0"
+}
+
+# Allow Flannel WireGuard (IPv6)
+resource "openstack_networking_secgroup_rule_v2" "allow_flannel_wireguard_ipv6" {
+  security_group_id = openstack_networking_secgroup_v2.vm_security_group.id
+  direction         = "ingress"
+  ethertype         = "IPv6"
+  protocol          = "udp"
+  port_range_min    = 51821
+  port_range_max    = 51821
+  remote_ip_prefix  = "::/0"
+}
+
+# Allow kubelet API
+resource "openstack_networking_secgroup_rule_v2" "allow_kubelet" {
+  security_group_id = openstack_networking_secgroup_v2.vm_security_group.id
+  direction         = "ingress"
+  ethertype         = "IPv4"
+  port_range_min    = 10250
+  port_range_max    = 10250
+  protocol          = "tcp"
+  remote_ip_prefix  = "0.0.0.0/0"
+}
+
+# Allow embedded distributed registry (TCP 5001)
+resource "openstack_networking_secgroup_rule_v2" "allow_embedded_registry_5001" {
+  security_group_id = openstack_networking_secgroup_v2.vm_security_group.id
+  direction         = "ingress"
+  ethertype         = "IPv4"
+  port_range_min    = 5001
+  port_range_max    = 5001
+  protocol          = "tcp"
   remote_ip_prefix  = "0.0.0.0/0"
 }
 
@@ -56,10 +111,69 @@ resource "openstack_networking_secgroup_rule_v2" "allow_nodeport" {
   remote_ip_prefix  = "0.0.0.0/0"
 }
 
-# Allow kubelet API
-resource "openstack_networking_secgroup_rule_v2" "allow_kubelet" {
+
+## ici
+
+
+# Allow embedded etcd (HA)
+resource "openstack_networking_secgroup_rule_v2" "allow_embedded_etcd_egress" {
   security_group_id = openstack_networking_secgroup_v2.vm_security_group.id
-  direction         = "ingress"
+  direction         = "egress"
+  ethertype         = "IPv4"
+  port_range_min    = 2379
+  port_range_max    = 2380
+  protocol          = "tcp"
+  remote_ip_prefix  = "0.0.0.0/0"
+}
+
+# Allow K3s API Server
+resource "openstack_networking_secgroup_rule_v2" "allow_kube_api_egress" {
+  security_group_id = openstack_networking_secgroup_v2.vm_security_group.id
+  direction         = "egress"
+  ethertype         = "IPv4"
+  port_range_min    = 6443
+  port_range_max    = 6443
+  protocol          = "tcp"
+  remote_ip_prefix  = "0.0.0.0/0"
+}
+
+# Allow Flannel VXLAN Networking
+resource "openstack_networking_secgroup_rule_v2" "allow_flannel_vxlan_egress" {
+  security_group_id = openstack_networking_secgroup_v2.vm_security_group.id
+  direction         = "egress"
+  ethertype         = "IPv4"
+  protocol          = "udp"
+  port_range_min    = 8472
+  port_range_max    = 8472
+  remote_ip_prefix  = "0.0.0.0/0"
+}
+
+# Allow Flannel WireGuard (IPv4)
+resource "openstack_networking_secgroup_rule_v2" "allow_flannel_wireguard_ipv4_egress" {
+  security_group_id = openstack_networking_secgroup_v2.vm_security_group.id
+  direction         = "egress"
+  ethertype         = "IPv4"
+  protocol          = "udp"
+  port_range_min    = 51820
+  port_range_max    = 51820
+  remote_ip_prefix  = "0.0.0.0/0"
+}
+
+# Allow Flannel WireGuard (IPv6)
+resource "openstack_networking_secgroup_rule_v2" "allow_flannel_wireguard_ipv6_egress" {
+  security_group_id = openstack_networking_secgroup_v2.vm_security_group.id
+  direction         = "egress"
+  ethertype         = "IPv6"
+  protocol          = "udp"
+  port_range_min    = 51821
+  port_range_max    = 51821
+  remote_ip_prefix  = "::/0"
+}
+
+# Allow kubelet API
+resource "openstack_networking_secgroup_rule_v2" "allow_kubelet_egress" {
+  security_group_id = openstack_networking_secgroup_v2.vm_security_group.id
+  direction         = "egress"
   ethertype         = "IPv4"
   port_range_min    = 10250
   port_range_max    = 10250
@@ -67,11 +181,34 @@ resource "openstack_networking_secgroup_rule_v2" "allow_kubelet" {
   remote_ip_prefix  = "0.0.0.0/0"
 }
 
-# Allow Egress Traffic (General)
-resource "openstack_networking_secgroup_rule_v2" "allow_egress" {
+# Allow embedded distributed registry (TCP 5001)
+resource "openstack_networking_secgroup_rule_v2" "allow_embedded_registry_5001_egress" {
+  security_group_id = openstack_networking_secgroup_v2.vm_security_group.id
+  direction         = "egress"
+  ethertype         = "IPv4"
+  port_range_min    = 5001
+  port_range_max    = 5001
+  protocol          = "tcp"
+  remote_ip_prefix  = "0.0.0.0/0"
+}
+
+# Allow NodePort Services
+resource "openstack_networking_secgroup_rule_v2" "allow_nodeport_egress" {
   security_group_id = openstack_networking_secgroup_v2.vm_security_group.id
   direction         = "egress"
   ethertype         = "IPv4"
   protocol          = "tcp"
+  port_range_min    = 30000
+  port_range_max    = 32767
   remote_ip_prefix  = "0.0.0.0/0"
 }
+
+
+# Allow Egress Traffic (General)
+# resource "openstack_networking_secgroup_rule_v2" "allow_egress" {
+#   security_group_id = openstack_networking_secgroup_v2.vm_security_group.id
+#   direction         = "egress"
+#   ethertype         = "IPv4"
+#   protocol          = null
+#   remote_ip_prefix  = "0.0.0.0/0"
+# }
